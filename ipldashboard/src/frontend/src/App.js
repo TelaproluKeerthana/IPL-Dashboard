2a998ad2f51d5ebe72dbda2e911d5aa9d26b875c
Keerthana Telaprolu <keerthanachowdary29@gmail.com>
2025-06-29 23:05:49 -0400

    Added Match page

--- ipldashboard/src/frontend/src/App.js
+++ ipldashboard/src/frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage/>
+      <Router>
+        <Routes>
+          <Route path = "/team/:teamName/matches/:year" element={<MatchPage/>}/>
+          <Route path = "/team/:teamName" element={<TeamPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ ipldashboard/src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,15 @@
+import {React, useEffect, useState} from 'react'; 
+import { MatchDetailCard } from '../components/MatchDetailCard';
+import { MatchSmallCard } from '../components/MatchSmallCard';
+import { useParams } from 'react-router-dom';
+export const MatchPage = () => {
+    
+    const [team, setTeam] = useState({matches: []});
+    const {teamName} = useParams();
+
+  return (
+    <div className="TeamPage">
+     <h1>Match Page</h1>
+    </div>
+  );
+}

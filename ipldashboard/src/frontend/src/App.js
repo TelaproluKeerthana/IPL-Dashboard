f8d665377962b31028b91f1ffeae1c1726d01e63
Keerthana Telaprolu <keerthanachowdary29@gmail.com>
2025-07-03 23:35:44 -0400

    front-end fixes updated css and added dashboard pages

--- ipldashboard/src/frontend/src/App.js
+++ ipldashboard/src/frontend/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.scss';
+import {Routes, Route} from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 import { MatchPage } from './pages/MatchPage';
+import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path = "/team/:teamName/matches/:year" element={<MatchPage/>}/>
-          <Route path = "/team/:teamName" element={<TeamPage />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/teams/:teamName/matches/:year" element = {<MatchPage />}/>
+        <Route path="/teams/:teamName" element = {<TeamPage />}/>
+        <Route path="/" element = {<HomePage />}/>
+
+      </Routes>
     </div>
   );
 }
